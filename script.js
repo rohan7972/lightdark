ed4dd5522d009cd5053663dd8ab68b82a3ef4976
rohan7972 <rohanpatil7972@gmail.com>
2023-12-30 21:02:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleTheme() {
+    const body = document.body;
+    const themeSwitchContainer = document.querySelector('.theme-switch-container');
+    const themeSwitch = document.getElementById('theme-switch');
+  
+    body.classList.toggle('light-mode');
+    body.classList.toggle('dark-mode');
+    themeSwitchContainer.classList.toggle('light-mode');
+    themeSwitchContainer.classList.toggle('dark-mode');
+  
+    // Change button text based on the current mode
+    const currentMode = body.classList.contains('dark-mode') ? 'Dark Mode' : 'Light Mode';
+    themeSwitch.textContent = currentMode;
+  }
+  
